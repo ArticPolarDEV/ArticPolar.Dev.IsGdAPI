e062e96f4cd543be093eedffff6c15fc4af67e83
JoseLucas1303 <74744598+JoseLucas1303@users.noreply.github.com>
2023-06-20 18:52:04 -0300

    Added Source Code

---
+++ IsGdAPI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArticPolar.Dev.IsGdAPI
+{
+    public class IsGdAPI
+    {
+        string apiUrl = "https://is.gd/create.php";
+
+        public async Task<string> ShortenUrlAsync(string longUrl, string customUrl = null)
+        {
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var parameters = "format=simple&url=" + Uri.EscapeDataString(longUrl);
+                if (!string.IsNullOrEmpty(customUrl))
+                {
+                    parameters += "&shorturl=" + Uri.EscapeDataString(customUrl);
+                }
+                
+                var response = await httpClient.GetAsync(apiUrl + "?" + parameters);
+                if (response.IsSuccessStatusCode)
+                {
+                    var shortenedUrl = await response.Content.ReadAsStringAsync();
+                    return shortenedUrl.Trim();
+                }
+                else
+                {
+                    MessageBox.Show("Error: " + response.StatusCode + ", verify if this link already exists!");
+                    return null;
+                }
+            }
+        }
+    }
+}
